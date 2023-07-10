2d45a99454f2bd7d3f0dd901cf0707c329032378
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-07-10 19:25:17 +0200

    bonusopdracht uitwerken

--- src/App.js
+++ src/App.js
@@ -2,70 +2,87 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [nameValue, setNameValue] = useState('');
-    const [ageValue, setAgeValue] = useState(0);
-    const [remarksValue, setRemarksValue] = useState('');
-    const [newsLetterChecked, toggleNewsLetterChecked] = useState(false);
+    const [formState, setFormState] = useState({
+        name: '',
+        age: 0,
+        remarks: '',
+        newsletter: false
+    })
+
+    // const [nameValue, setNameValue] = useState('');
+    // const [ageValue, setAgeValue] = useState(0);
+    // const [remarksValue, setRemarksValue] = useState('');
+    // const [newsLetterChecked, toggleNewsLetterChecked] = useState(false);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(nameValue, ageValue, remarksValue, newsLetterChecked);
+        console.log(formState);
+    }
+
+    function handleFormChange(e) {
+        const changedFieldName = e.target.name;
+        const newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        setFormState({
+            ...formState,
+            [changedFieldName]: newValue,
+        });
     }
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <fieldset>
-                <legend>Gegevens</legend>
-                <label htmlFor="form-name">
-                    Naam:
-                    <input type="text"
-                           id="form-name"
-                           name="name"
-                           value={nameValue}
-                           onChange={(e) => setNameValue(e.target.value)}
-                    />
-                </label>
-                <label htmlFor="form-age">
-                    Leeftijd:
-                    <input type="text"
-                           id="form-age"
-                           name="age"
-                           value={ageValue}
-                           onChange={(e) => setAgeValue(parseInt(e.target.value))}
-                    />
-                </label>
-            </fieldset>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>Gegevens</legend>
+                    <label htmlFor="form-name">
+                        Naam:
+                        <input type="text"
+                               id="form-name"
+                               name="name"
+                               value={formState.name}
+                               onChange={handleFormChange}
+                        />
+                    </label>
+                    <label htmlFor="form-age">
+                        Leeftijd:
+                        <input type="text"
+                               id="form-age"
+                               name="age"
+                               value={formState.age}
+                               onChange={handleFormChange}
+                        />
+                    </label>
+                </fieldset>
 
-            <fieldset>
-                <legend>Jouw review</legend>
-                <label htmlFor="form-name">
-                    Opmerkingen:
-                    <textarea id="form-remarks"
-                              name="name"
-                              value={remarksValue}
-                              onChange={(e) => setRemarksValue(e.target.value)}
-                              cols="50"
-                              rows="5"
-                              placeholder="Wat vond je van het recept?"
-                    >
+                <fieldset>
+                    <legend>Jouw review</legend>
+                    <label htmlFor="form-name">
+                        Opmerkingen:
+                        <textarea id="form-remarks"
+                                  name="remarks"
+                                  value={formState.remarks}
+                                  onChange={handleFormChange}
+                                  cols="50"
+                                  rows="5"
+                                  placeholder="Wat vond je van het recept?"
+                        >
                     </textarea>
-                </label>
-                <label htmlFor="form-newsletter">
-                    <input type="checkbox"
-                           id="form-newsletter"
-                           name="toggle-newsletter"
-                           checked={newsLetterChecked}
-                           onChange={() => toggleNewsLetterChecked(!newsLetterChecked)}
-                    />
-                    Ik schrijf me in voor de nieuwsbrief!
-                </label>
+                    </label>
+                    <label htmlFor="form-newsletter">
+                        <input type="checkbox"
+                               id="form-newsletter"
+                               name="newsletter"
+                               checked={formState.newsletter}
+                               onChange={handleFormChange}
+                        />
+                        Ik schrijf me in voor de nieuwsbrief!
+                    </label>
 
-            </fieldset>
-            <button type="submit">
-                Versturen
-            </button>
-        </form>
+                </fieldset>
+                <button type="submit">
+                    Versturen
+                </button>
+            </form>
         </div>
     );
 }
